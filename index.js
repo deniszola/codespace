2a04f3b119cc13c3a118c5f8f7c8115b48fc0ef4
Denis Batista de Andrade <34269902+deniszola@users.noreply.github.com>
2022-05-30 09:46:01 +0000

    refactor: :recycle: clean-up and organize `index.js` file

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-let express = require("express");
-let app = express();
-let ejs = require("ejs");
+const express = require("express");
 const haikus = require("./haikus.json");
+
+const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static("public"));
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index", { haikus: haikus });
